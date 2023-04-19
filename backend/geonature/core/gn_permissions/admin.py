aec8daf9e997273f6a962b3cb6792c31277bd374
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-04-19 16:43:14 +0200

    permissions: add sensitivity filter

--- backend/geonature/core/gn_permissions/admin.py
+++ backend/geonature/core/gn_permissions/admin.py
@@ -572,6 +572,7 @@ class PermissionAvailableAdmin(CruvedProtectedMixin, ModelView):
         "scope": "Portée",
         "object": "Objet",
         "scope_filter": "Filtre appartenance",
+        "sensitivity_filter": "Filtre sensibilité",
     }
     column_formatters = {
         "module": lambda v, c, m, p: m.module.module_code,
