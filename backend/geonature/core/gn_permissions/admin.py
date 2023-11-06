f538afb53678c4acfd642fa540383b97d5ca8fe1
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-11-06 17:24:23 +0100

    feat(api): admin: change label depending on param
    If activated: blur, otherwise: hide

--- backend/geonature/core/gn_permissions/admin.py
+++ backend/geonature/core/gn_permissions/admin.py
@@ -9,6 +9,7 @@ from flask_admin.form.widgets import Select2Widget
 from sqlalchemy.orm import contains_eager, joinedload
 
 from geonature.utils.env import db
+from geonature.utils.config import config
 from geonature.core.admin.admin import admin
 from geonature.core.admin.utils import CruvedProtectedMixin, DynamicOptionsMixin
 from geonature.core.gn_permissions.models import (
@@ -353,7 +354,7 @@ class PermissionAdmin(CruvedProtectedMixin, ModelView):
         "role.nom_complet": "nom du rôle",
         "availability": "Permission",
         "scope": "Filtre sur l'appartenance des données",
-        "sensitivity_filter": "Flouter les données sensibles",
+        "sensitivity_filter": ("Flouter" if config["SYNTHESE"]["BLUR_SENSITIVE_OBSERVATIONS"] else "Masquer") + " les données sensibles",
     }
     column_select_related_list = ("availability",)
     column_searchable_list = ("role.identifiant", "role.nom_complet")
